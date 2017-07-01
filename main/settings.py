d4f0c47db4eee54674f0905f06270f144082c0c2
Md Rahaman <soikatesc@gmail.com>
2017-07-01 17:37:28 -0500

    added header file

--- main/settings.py
+++ main/settings.py
@@ -126,5 +126,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, "static/") if envp=='deployment' else STATIC_ROOT
 if envp=='deployment':
     STATIC_ROOT = os.path.join(BASE_DIR, "static/")
